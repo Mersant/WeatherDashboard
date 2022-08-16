5ddabc1df3889469d766afcba3e7f9748da901fb
Mersant <dlowry99@outlook.com>
2022-08-16 12:02:38 -0400

    Added search bar history functionality
    When the user manually types in a city name and searches for it, it is now added to a history list stored in localStorage. The left sidebar list items are now clickable for the user to quickly search for past cities. The history does not update, purposefully, when the user clicks on past search results.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,47 @@
+// citySearchHistory is a clone of the value in localStorage, if it exists, and is an array containing all of the past cities the user searched for the weather in. Its values are displayed on the left sidebar so the user can quickly select cities to search for.
+var citySearchHistory = []
+if(!localStorage.getItem("citySearchHistory")) {
+    citySearchHistory = ["Portland", "Dayton", "Columbus", "New York", "San Diego"];
+    localStorage.setItem("citySearchHistory", JSON.stringify(citySearchHistory));
+} else {
+    citySearchHistory = JSON.parse(localStorage.citySearchHistory);
+}
+function updateLi() {
+    var listItems = $("#citySearchList li");
+    listItems.each(function(i, li) {
+        $(li).text(citySearchHistory[i]);
+        $(li).on("click", function() {
+            $("#citySelect").val(citySearchHistory[i])
+            updateForecasts();
+        })
+    });
+}
+updateLi();
 
-$("#searchCity").on("click", updateToday);
 
-function updateToday() {
+$("#searchCity").on("click", function() {
+    if($("#citySelect").val() != "") {
+        // Update city search list, then update the weather forecasts.
+        citySearchHistory.unshift($("#citySelect").val());
+        citySearchHistory.length = 5; // Prevent history from ballooning as time goes on
+        localStorage.setItem("citySearchHistory", JSON.stringify(citySearchHistory));
+        updateLi();
+        updateForecasts();
+    }
+    else {
+        alert("Invalid city name!");
+    }
+});
+
+// This function updates today's forecasts as well as the 5-day forecast.
+function updateForecasts() {
     if($("#citySelect").val() != "") {
         $.ajax({
             type: "POST",
             url: "https://api.openweathermap.org/data/2.5/forecast?q=" + $("#citySelect").val() + "&exclude=hourly,daily&appid=2f5b5012d822c727f2a2149316cd61c6&units=imperial&cnt=6",
             dataType: "json",
             success: function (result, status, xhr) {
-                // Reset h1 so it contains the city name and today's date
+                // Reset the header in today's forecast so it contains the city name and today's date
                 var now = moment();
                 $("#cityNameDate").text( $("#citySelect").val() + ", Today (" + now.format('MMMM Do YYYY') + ")" );
                 $("#cityTemp").text(result["list"][0]["main"]["temp"]);
@@ -30,7 +63,7 @@ function updateToday() {
         });
     }
 }
-updateToday();
+updateForecasts();
 
 // Make seperate API call to update UVI
 function updateUVI(lat, lon) {
@@ -40,6 +73,7 @@ function updateUVI(lat, lon) {
         dataType: "json",
         success: function (result, status, xhr) {
             $("#cityUVIndex").text(result["value"]);
+            // Color code the UV index in terms of how dangerous it is
             if( result["value"] < 2 ) {
                 $("#cityUVIndex").css("background-color", "green");
                 $("#cityUVIndex").css("color", "white");
@@ -63,9 +97,7 @@ function updateUVI(lat, lon) {
             
         },
         error: function (xhr, status, error) {
-            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText + ". Please retype the name of the city")
+            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
         }
     })
 }
-
-// Finally, update the daily forecast
